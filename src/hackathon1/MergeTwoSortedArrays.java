21903a124be57f52d4b6b73aac745849934ba038
Shilpa <shilpa.dursheti21@gmail.com>
2022-11-23 19:01:42 -0800

    my fifth commit adding src folder

---
+++ src/hackathon1/MergeTwoSortedArrays.java
@@ -0,0 +1,49 @@
+//Q26. WJP to merge two sorted array.(Do not use third array)	
+	//array1[6] = 1,2,4,6,9,10
+	//array2[4] =  3, 5,7,8
+	//After merge :  array1[10] = 1,2,3,4,5,6,7,8,9,10
+
+package hackathon1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NavigableMap;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class MergeTwoSortedArrays {
+	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Enter array size = ");
+		int size = Sc.nextInt();
+		int A[]=new int[size];
+		System.out.println("Enter the numbers = ");
+		for(int i=0;i<size;i++) {
+			A[i]=Sc.nextInt();
+		}
+		System.out.println("Enter array size = ");
+		int size1 = Sc.nextInt();
+		int B[]=new int[size1];
+		System.out.println("Enter the numbers = ");
+		for(int i=0;i<size;i++) {
+			B[i]=Sc.nextInt();
+		}
+		ArrayList<Integer> Al = new ArrayList<Integer>(Arrays.asList(A));
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+
+
